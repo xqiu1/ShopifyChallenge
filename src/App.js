588dff7f684cf7fe9b44a5d93c83398c29febfc6
Estelle Qiu <44105167+xqiu1@users.noreply.github.com>
2022-01-09 11:25:28 -0800

    Update app

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "antd/dist/antd.css";
+import "./App.css";
+import {Gallery} from "./components/Gallery";
+
+function App() {
+  return (
+    <div className="App">
+      <Gallery />
+    </div>
+  );
+}
+
+export default App;
